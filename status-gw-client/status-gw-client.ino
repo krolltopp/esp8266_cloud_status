b58e564da280095b55816f3017114bafbcad2fa2
krolltopp <krolltopp@gmail.com>
2016-02-10 19:43:05 +0100

    Added status gw client

---
+++ status-gw-client/status-gw-client.ino
@@ -0,0 +1,75 @@
+/**
+   BasicHTTPClient.ino
+
+    Created on: 24.05.2015
+
+*/
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {
+
+  USE_SERIAL.begin(115200);
+  // USE_SERIAL.setDebugOutput(true);
+
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
+  }
+
+  WiFiMulti.addAP("Tenda_F31168");
+}
+
+void loop() {
+  DynamicJsonBuffer jsonBuffer;
+  // wait for WiFi connection
+
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    HTTPClient http;
+
+    USE_SERIAL.print("[HTTP] begin...\n");
+    // configure traged server and url
+    //http.begin("192.168.1.12", 443, "/test.html", true, "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+    http.begin("status-gw.herokuapp.com", 80, "/api/status"); //HTTP
+
+    USE_SERIAL.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+    if (httpCode) {
+      // HTTP header has been send and Server response header has been handled
+      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == 200) {
+        String json = http.getString();
+        USE_SERIAL.println(json);
+        JsonObject& root = jsonBuffer.parseObject(json);
+        if (root.success()) {
+          USE_SERIAL.println("Parsing Success");
+          const char* mainColor = root["mainColor"];
+          USE_SERIAL.printf("mainColor: %s\n", mainColor);
+        }
+      }
+    } else {
+      USE_SERIAL.print("[HTTP] GET... failed, no connection or no HTTP server\n");
+    }
+  }
+
+  delay(10000);
+}
+
